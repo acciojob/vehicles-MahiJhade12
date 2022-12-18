0ab35f6e5a92c1ad4668befae68a87326dbb5479
mahima <mahimajhade123@gmail.com>
2022-12-18 15:10:47 +0530

    done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -11,27 +11,17 @@ public class F1 extends Car {
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
 
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+
         newSpeed=getCurrentSpeed()+rate;
         if(newSpeed == 0) {
             changeGear(1);
             stop();
             //Stop the car, set gear as 1
         }
-        if(newSpeed>=1 && newSpeed<=50){
-
-        }
-        else if(newSpeed>0 && newSpeed<=50){
+       else if(newSpeed>0 && newSpeed<=50){
             changeGear(1);
-        }else if(newSpeed>=51 && newSpeed<=100){
+        }
+       else if(newSpeed>=51 && newSpeed<=100){
             changeGear(2);
         }else if(newSpeed>=101 && newSpeed<=150){
             changeGear(3);
